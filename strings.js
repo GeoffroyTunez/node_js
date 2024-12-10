097d85bae274319bd734126d94999fd870e515da
Allan <allan.bernier1@gmail.com>
2024-12-10 10:38:37 +0100

    Nombres & strings

---
+++ strings.js
@@ -0,0 +1,18 @@
+const name = "Allan"
+const surname = 'Allan'
+const birthdate = `Allan`
+const age = 35
+
+console.log("Bonjour " + name + ' Vous avez ' + age + " ans")
+
+console.log(`Bonjour ${surname} vous avez ${age} ans.`)
+
+
+console.log(surname.toUpperCase())
+console.log(surname.toLowerCase())
+console.log(surname.length)
+
+console.log(surname.includes("Al"))
+console.log(surname.includes("aa"))
+
+console.log(surname.charAt(3))
--- variables.js
+++
@@ -1,4 +0,0 @@
-var a = 1;
-let b = 2;
-const c = 3;
-
